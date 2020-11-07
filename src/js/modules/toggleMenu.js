ea166a6bb4123aad7773543e097a5993be4e3e1c
Mikhail Shuvarov <shuvarov.md@gmail.com>
2020-11-07 22:34:56 +0300

    feat: create toggleMenu.js. Fixed menu.scss, showThanks.js

---
+++ src/js/modules/toggleMenu.js
@@ -0,0 +1,19 @@
+const toggleMenu = () => {
+  const menuSpan = document.querySelectorAll('.menu__span'),
+    menuList = document.querySelector('.menu__list');
+
+  menuSpan.forEach(item => {
+    item.classList.toggle('menu__span--active');
+  });
+  menuList.classList.toggle('menu__list--active');
+};
+
+document.body.addEventListener('click', event => {
+  const menuActive = document.querySelector('.menu__list--active');
+
+  if (event.target.closest('.menu__burger') || event.target.closest('.menu__item')) {
+    toggleMenu();
+  } else if (menuActive && !event.target.closest('.menu__list--active')) {
+    toggleMenu();
+  }
+});
